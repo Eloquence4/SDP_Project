870fe21b4c91baf45bbde5170b19bc056b52391f
Eloquence4 <zelin11@live.com>
2017-01-23 20:25:20 +0200

    Hopefully finished with all the archiving, needs testing

--- Header/Archive.h
+++ Header/Archive.h
@@ -1,21 +1,33 @@
 #ifndef ArchiveDec
 #define ArchiveDec
 
+#include <string> // For strcmp() and strlen()
 #include <fstream>
 #include "BinaryTree.h"
 #include "BitVector.h"
 #include "PriorityQueue.h"
 #include "Vector.h"
+#include "dirent.h"
+
+enum File_Folder_States
+{
+    NO_STATE,
+    DIRECTORY_START,
+    DIRECTORY_END,
+    FILE_START
+};
 
 namespace Archive
 {
+    bool CompressFolder(std::fstream& archive, const char* folder_name);
+
     BinaryTree ConstructHuffmanTree(std::fstream& file);
 
     void FillWeightVector(std::fstream& file, Vector& data);
 
     void FillMetaData(std::fstream& file, const BinaryTree& HuffmanTree, const char* fileName, size_t fileNameLen);
 
-    void Compress(std::fstream& from, std::fstream& target, const BinaryTree& HuffmanTree);
+    void CompressFile(std::fstream& from, std::fstream& target, const BinaryTree& HuffmanTree);
 }
 
 #endif // ArchiveDec
--- Source/Archive.cpp
+++ Source/Archive.cpp
@@ -1,5 +1,51 @@
 #include "../Header/Archive.h"
 
+bool Archive::CompressFolder(std::fstream& archive, const char* folder_name)
+{
+    File_Folder_States state = NO_STATE;
+    DIR* pDir = opendir(folder_name);
+
+    if(!pDir) // folder_name is not a valid path to a folder, is probably a file
+        return false;
+
+    // Metadata for the folder
+    state = DIRECTORY_START;
+    unsigned file_name_size = strlen(folder_name);
+    archive.write((char*) &state, sizeof(state));
+    archive.write((char*) &file_name_size, sizeof(file_name_size));
+    archive.write(folder_name, file_name_size);
+    ///
+
+    struct dirent* pent = nullptr;
+
+    while(pent = readdir(pDir))
+    {
+        if(!pent)
+        {
+            state = DIRECTORY_END;
+            archive.write((char*)&state, sizeof(state));
+            closedir(pDir);
+            return true; // Successfully traversed the folder and archived all the files
+        }
+
+        if(strcmp(pent->d_name, ".") == 0)
+            continue;
+        if(strcmp(pent->d_name, "..") == 0)
+            continue;
+
+        // Will return false only if this is a file
+        // If pend->d_name is a folder, it will recursively archive all the files in it
+        if(CompressFolder(archive, pent->d_name))
+            continue;
+
+        // The loop would execture this code only if pent->d_name is a file
+        std::fstream file(pent->d_name, std::ios::in);
+        BinaryTree HuffmanTree = ConstructHuffmanTree(file);
+        FillMetaData(archive, HuffmanTree, pent->d_name, pent->d_namlen);
+        CompressFile(file, archive, HuffmanTree);
+    }
+}
+
 BinaryTree Archive::ConstructHuffmanTree(std::fstream& file)
 {
     file.clear();
@@ -42,12 +88,14 @@ void Archive::FillWeightVector(std::fstream& file, Vector& data)
 
 void Archive::FillMetaData(std::fstream& file, const BinaryTree& HuffmanTree, const char* fileName, size_t fileNameLen)
 {
+    File_Folder_States state = FILE_START;
+    file.write((char*) &state, sizeof(state));
     file.write((char*) &fileNameLen, sizeof(size_t));
     file.write(fileName, fileNameLen);
     HuffmanTree.BinaryExport(file);
 }
 
-void Archive::Compress(std::fstream& from, std::fstream& target, const BinaryTree& HuffmanTree)
+void Archive::CompressFile(std::fstream& from, std::fstream& target, const BinaryTree& HuffmanTree)
 {
     from.clear();
     from.seekg(0, std::ios::beg);
